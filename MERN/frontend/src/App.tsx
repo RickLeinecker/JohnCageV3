c3ce162a4f1d94267a990ea942c8572df2e4a332
KyleM2349 <kylemason@knights.ucf.edu>
2023-03-07 22:02:04 -0500

    Typescript Project

---
+++ MERN/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AudioList from "./AudioList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <AudioList />
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.post('/api/test', async (req, res, next) => 
+{
+  res.status(200).json({return:"success"});
+});
+
+app.listen(5000);
