59e8cdd9e7e1c0116545db44162f8a65677b979d
KyleM2349 <kylemason@knights.ucf.edu>
2023-06-18 14:20:03 -0400

    MySQL integration from Fall Semester

--- MERN/frontend/src/App.tsx
+++ MERN/frontend/src/App.tsx
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import AudioList from "./AudioList";
+import ReactAudioPlayer from "react-audio-player";
+import { Container, Row, Form, Card, Col, Dropdown, ButtonGroup, Button, Table, Modal } from "react-bootstrap";
+//import Collapsible from "react-collapsible";
+
+interface result {
+  name: string,
+  id: number
+}
+
+function buildPath(route) {
+  return 'http://localhost:5000/' + route;
+}
 
 function App() {
+  const [searchList, setSearchList] = useState<Array<result>>([{ name: 'one', id: 1 }]);
+  const [searchText, setSearchText] = useState<string>('');
+
+  const performSearch = async function () {
+    try {
+      //Get recording metadata according to search text
+      const response = await fetch(buildPath('api/searchSongs?search=' + searchText), { method: 'POST', headers: { 'Content-Type': 'application/json' } });
+      console.log(buildPath('api/searchSongs?search=' + searchText));
+      var res = JSON.parse(await response.text());
+      var sd = JSON.parse(JSON.stringify(res));
+      const searchResults = sd.searchResults;
+
+      //Save metadata to "result" interface array
+      var searchTemp: result[] = [];
+      for (var i = 0; i < searchResults.length; ++i) {
+        searchTemp.push({ name: searchResults[i].Title, id: searchResults[i].ID });
+      }
+
+      //Save metadata to page for display
+      console.log(searchTemp);
+      setSearchList(searchTemp);
+    }
+    catch (e) {
+      alert(e.toString());
+      return;
+    }
+  };
+
+  const SearchChangeHandler = (event) => {
+    event.preventDefault();
+    setSearchText(event.target.value);
+  };
+
+  useEffect(() => {
+    performSearch();
+  }, [searchText]);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <AudioList />
+    <div className="Search Page">
+      <h1>Search for a Recording</h1>
+      <Form.Group>
+        <Form.Control type='searchtext' value={searchText} onChange={SearchChangeHandler} placeholder="Search" />
+      </Form.Group>
+
+      <div className="Audio List">
+        {searchList.map((key, index) => {
+          return (
+            <Row>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>{searchList[index].name}</Card.Title>
+                  <ReactAudioPlayer src={'http://localhost:5000/api/getSong?id=' + String(searchList[index].id)} controls />
+                </Card.Body>
+              </Card>
+            </Row>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- MERN/server.js
+++ MERN/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const ms = require('mediaserver');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => 
-{
+//Required for Jest unit testing.
+module.exports = app;
+
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'New',
+  database: 'JCT',
+  password: 'MyN3wP4ssw0rd'
+});
+
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -20,9 +30,40 @@ app.use((req, res, next) =>
   next();
 });
 
-app.post('/api/test', async (req, res, next) => 
-{
-  res.status(200).json({return:"success"});
+app.post('/api/searchSongs', async (req, res, next) => {
+  var searchString = req.query["search"];
+  var sqlSearch;
+  if (searchString) {
+    sqlSearch = "SELECT ID, Title FROM Recordings WHERE Title like '%" + searchString + "%'";
+  }
+  else {
+    sqlSearch = "SELECT ID, Title FROM Recordings WHERE Title like '%%'";
+  }
+
+  pool.execute(sqlSearch, function (err, result) {
+    if (err) throw err;
+    res.status(200).send({ searchResults: result });
+  });
 });
+
+app.get('/api/getSong', async (req, res, next) => {
+  const songId = req.query["id"];
+  let sqlSelect = "SELECT Title FROM Recordings WHERE ID=" + songId;
+  var fileName = "";
+
+  console.log(sqlSelect);
+
+  pool.execute(sqlSelect, function (err, result) {
+    if (err) throw err;
+    if (result[0]) {
+      fileName = result[0].Title;
+      ms.pipe(req, res, './Music/' + fileName + '.mp3');
+      //res.status(200);
+    }
+  });
+});
+
+//CREATE USER 'NewUser'@'localhost' IDENTIFIED BY '3atth!s!';
+//CREATE USER 'New'@'localhost' IDENTIFIED BY 'MyN3wP4ssw0rd';
 
 app.listen(5000);
