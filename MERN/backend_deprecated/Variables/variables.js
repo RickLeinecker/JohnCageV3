f4c90b19bd435f5b7bfdac33c0192a0b859bba37
Kyle <kyle@pop-os.localdomain>
2023-09-26 19:49:54 -0400

    Socket prototype revision 999

--- MERN/backend_deprecated/Variables/variables.js
+++
@@ -1,5 +0,0 @@
-const isServerEnvironment = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-    socketCORS: string = isServerEnvironment ? 'http://localhost:3000' : 'https://johncagetribute.org'
-};
