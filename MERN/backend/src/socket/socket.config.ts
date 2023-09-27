545d642c8150a2bdfdf0407a6538f4c6b99e5053
Kyle <kyle@pop-os.localdomain>
2023-09-27 19:36:21 -0400

    Stashing

--- MERN/backend/src/socket/socket.config.ts
+++ MERN/backend/src/socket/socket.config.ts
@@ -1,10 +1,10 @@
-// // 64,000 bytes is about one second of 32kHz 16bit Mono PCM(raw) audio
-// const outgoingAudioChunkSize = 64000;
+// 64,000 bytes is about one second of 32kHz 16bit Mono PCM(raw) audio
+const outgoingAudioChunkSize = 64000;
 
-// // 40,000,000 bytes is a little over 10 minutes of 32kHz 16bit Mono PCM(raw) audio
-// const maxAudioBufferSize = 40000000;
+// 40,000,000 bytes is a little over 10 minutes of 32kHz 16bit Mono PCM(raw) audio
+const maxAudioBufferSize = 40000000;
 
-const outgoingAudioChunkSize = 4;
-const maxAudioBufferSize = 50;
+// const outgoingAudioChunkSize = 6;
+// const maxAudioBufferSize = 50;
 
 export { outgoingAudioChunkSize, maxAudioBufferSize };
