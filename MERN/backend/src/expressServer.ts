a4f28837f4a38fefc9e994e6b297eca1f00afecb
Kyle <kyle@pop-os.localdomain>
2023-10-03 21:21:50 -0400

    Trying express config for CORS.

--- MERN/backend/src/expressServer.ts
+++ MERN/backend/src/expressServer.ts
@@ -11,6 +11,18 @@ class expressServer {
     this.config(this.expressApp);
     this.syncDatabase();
     new Routes(this.expressApp);
+    this.expressApp.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      );
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+      );
+      next();
+    });
   }
 
   private config(app: Application): void {
