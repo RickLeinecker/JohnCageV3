0950d45c703fd7205578cc636d465783d26b92d2
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-10-12 13:31:08 -0400

    updated Take Credentials to use fetch responses

--- MERN/frontend/src/API/takeCredentialsAPI.tsx
+++ MERN/frontend/src/API/takeCredentialsAPI.tsx
@@ -4,6 +4,11 @@ const takeCredentials = async function name(email:string, password:string) {
     try{
         const JSONObj = JSON.stringify({"Email":email,"Password":password});
         console.log("Successfully created a JSON of login "+JSONObj);
+        const URL = buildPath("/users/login");
+        console.log("Fetch request URL:", URL);
+        const response = await fetch(URL,{ method: 'POST', mode: "cors", headers: { 'Content-Type': 'application/json' },body: JSONObj })
+        console.log("Response "+response.json);
+        return response.json;
     }
     catch(e)
     {
