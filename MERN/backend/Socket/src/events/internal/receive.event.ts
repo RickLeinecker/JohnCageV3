3e74aa8355ca6997512356a1fa969abf41863121
Kyle <kyle@pop-os.localdomain>
2023-10-28 22:57:06 -0400

    Account for an audio message of size 0 to prevent crash.

--- MERN/backend/Socket/src/events/internal/receive.event.ts
+++ MERN/backend/Socket/src/events/internal/receive.event.ts
@@ -5,7 +5,7 @@ const receiveAudio = function (performer: Performer, rawAudio: ArrayBuffer) {
     // Write message contents into user's buffer.
     let thisView = new DataView(rawAudio);
     let performerData = performer.data;
-    for (let i = 0; i < Math.floor((thisView.byteLength) / 2) * 2; ++i) {
+    for (let i = 0; i < thisView.byteLength; ++i) {
         performerData.audioBuffer.writeUint8(thisView.getUint8(i), performerData.bufferSize);
         performerData.bytesLefttoProcess++;
         performerData.bufferSize++;
