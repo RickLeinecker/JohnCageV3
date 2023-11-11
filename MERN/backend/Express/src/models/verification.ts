14bc0cde0af126dc5c924056b4179008daf24e31
Kyle <kyle@pop-os.localdomain>
2023-11-10 21:06:05 -0500

    New models

--- MERN/backend/Express/src/models/verification.ts
+++ MERN/backend/Express/src/models/verification.ts
@@ -3,16 +3,19 @@ import { DataTypes, Model, Optional } from 'sequelize';
 import type { users, usersId } from './users';
 
 export interface verificationAttributes {
+  ID: number;
   VerificationCode?: number;
   UserID?: number;
   DateCreated?: Date;
 }
 
-export type verificationId = verification["UserID"];
-export type verificationOptionalAttributes = "VerificationCode" | "UserID" | "DateCreated";
+export type verificationPk = "ID";
+export type verificationId = verification[verificationPk];
+export type verificationOptionalAttributes = "ID" | "VerificationCode" | "UserID" | "DateCreated";
 export type verificationCreationAttributes = Optional<verificationAttributes, verificationOptionalAttributes>;
 
 export class verification extends Model<verificationAttributes, verificationCreationAttributes> implements verificationAttributes {
+  ID!: number;
   VerificationCode?: number;
   UserID?: number;
   DateCreated?: Date;
@@ -25,36 +28,50 @@ export class verification extends Model<verificationAttributes, verificationCrea
 
   static initModel(sequelize: Sequelize.Sequelize): typeof verification {
     return verification.init({
-      VerificationCode: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      UserID: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: 'users',
-          key: 'ID'
-        }
-      },
-      DateCreated: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    ID: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    VerificationCode: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    UserID: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'ID'
       }
-    }, {
-      sequelize,
-      tableName: 'verification',
-      timestamps: false,
-      indexes: [
-        {
-          name: "UserID_idx",
-          using: "BTREE",
-          fields: [
-            { name: "UserID" },
-          ]
-        },
-      ]
-    });
+    },
+    DateCreated: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    tableName: 'verification',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "ID" },
+        ]
+      },
+      {
+        name: "UserID_idx",
+        using: "BTREE",
+        fields: [
+          { name: "UserID" },
+        ]
+      },
+    ]
+  });
   }
 }
